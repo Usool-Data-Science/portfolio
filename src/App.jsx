431d68720af475b356904b9e7a2e074cc5ac160d
Usool-Data-Science <adeshinaibrahim10@gmail.com>
2024-10-30 17:52:58 +0100

    update

--- src/App.jsx
+++ src/App.jsx
@@ -14,21 +14,30 @@ function App() {
   const [searchResult, setSearchResult] = useState([]);
   const [projectResult, setProjectResult] = useState([]);
 
+  function sortByDate(arr, dateProp, descending = false) {
+    return arr.sort((a, b) => {
+      const a_date = new Date(a[dateProp]);
+      const b_date = new Date(b[dateProp]);
+
+      return descending ? b_date - a_date : a_date - b_date;
+    })
+  }
+
   useEffect(() => {
     const result = POSTS.filter(post => (
       (post.title.toLowerCase()).includes(searchPost.toLowerCase()) ||
       (post.headline.toLowerCase()).includes(searchPost.toLowerCase())
     ));
+    sortByDate(result, "datetime", true);
     setSearchResult(result.length ? result :
       searchPost.length ? [] : POSTS);
   }, [searchPost])
 
   useEffect(() => {
     const result = PROJECTS.filter(project => (
-      // (project.title.toLowerCase()).includes(searchProject.toLowerCase()) ||
-      // (project.headline.toLowerCase()).includes(searchProject.toLowerCase())
       (project.stack_string.toLowerCase()).includes(searchProject.toLowerCase())
     ));
+    sortByDate(result, "datetime", true)
     setProjectResult(result.length ? result :
       searchProject.length ? [] : PROJECTS);
   }, [searchProject])
@@ -48,7 +57,6 @@ function App() {
             setSearchProject={setSearchProject}
             projects={projectResult}
           />} />
-          {/* <Route path='/resume' element={<Resume />} /> */}
         </Route>
       </Routes>
     </div>
